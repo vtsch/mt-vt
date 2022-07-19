7fab249e384dbeb292d734d09a659dc8b5eaa7ea
Vanessa Tschichold <vanessa@tschichold.com>
2022-07-19 15:12:50 +0200

    add confusion matrix

--- metrics.py
+++ metrics.py
@@ -1,21 +1,24 @@
 import torch
 import numpy as np
-from sklearn.metrics import accuracy_score, auc, f1_score, adjusted_mutual_info_score, mutual_info_score, rand_score, adjusted_rand_score, normalized_mutual_info_score
+import pandas as pd
+from sklearn.metrics import accuracy_score, auc, f1_score, mutual_info_score, rand_score, adjusted_rand_score, normalized_mutual_info_score, confusion_matrix
 
 def calculate_clustering_scores(y_true, y_pred, experiment):
     accuracy = accuracy_score(y_true, y_pred)
     ri = rand_score(y_true, y_pred)
     ari = adjusted_rand_score(y_true, y_pred)
     mi = mutual_info_score(y_true, y_pred)
-    ami = adjusted_mutual_info_score(y_true, y_pred)
     nmi = normalized_mutual_info_score(y_true, y_pred)
+    cm = confusion_matrix(y_true, y_pred)
     print('Clustering Accuracy: {:.3f}'.format(accuracy))
     print('Clustering RI: {:.3f}'.format(ri))
     print('Clustering ARI: {:.3f}'.format(ari))
     print('Clustering MI: {:.3f}'.format(mi))
-    print('Clustering AMI: {:.3f}'.format(ami))
     print('Clustering NMI: {:.3f}'.format(nmi))
-    experiment.log_metrics({"Clustering Accuracy": accuracy, "Clustering RI": ri, "Clustering ARI": ari, "Clustering MI": mi, "Clustering AMI": ami, "Clustering NMI": nmi})
+    print('Confusion Matrix:')
+    print(cm)
+    experiment.log_metrics(pd.DataFrame({'accuracy': [accuracy], 'ri': [ri], 'ari': [ari], 'mi': [mi], 'nmi': [nmi]}))
+    experiment.log_metrics(pd.DataFrame(cm), 'confusion_matrix')
 
 
 class Meter:
