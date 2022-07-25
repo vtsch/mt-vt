ea3a81d653906358ea77dbf27a5492be017b9aff
Vanessa Tschichold <vanessa@tschichold.com>
2022-07-25 17:08:53 +0200

    add another transformer to learn embeddings

--- transformer.py
+++ transformer.py
@@ -1,7 +1,6 @@
 from typing import Optional, Any
 import math
 import torch
-from torch import nn, Tensor
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.nn.modules import MultiheadAttention, Linear, Dropout, BatchNorm1d, TransformerEncoder, TransformerEncoderLayer
@@ -126,3 +125,105 @@ class Transformer(nn.Module):
         x = torch.sigmoid(x)
 
         return x
+
+class CausalConv1d(torch.nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=0,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+        
+        self.__padding = (kernel_size - 1) * dilation
+        
+    def forward(self, input):
+        return super(CausalConv1d, self).forward(F.pad(input, (self.__padding, 0)))
+
+
+class context_embedding(torch.nn.Module):
+    def __init__(self,in_channels=1,embedding_size=12,k=3):
+        super(context_embedding,self).__init__()
+        self.causal_convolution = CausalConv1d(in_channels,embedding_size,kernel_size=k)
+
+    def forward(self,x):
+        x = self.causal_convolution(x)
+        return torch.tanh(x)
+
+class TransformerTimeSeries(torch.nn.Module):
+    # https://github.com/sunnyqiny/Unsupervised-Temporal-Embedding-and-Clustering/tree/cc5a41df905efbac11788a43b6151c08c68b8c6c 
+    """
+    Time Series application of transformers based on paper
+    causal_convolution_layer parameters:
+        in_channels: the number of features per time point
+        out_channels: the number of features outputted per time point
+        kernel_size: k is the width of the 1-D sliding kernel
+    nn.Transformer parameters:
+        d_model: the size of the embedding vector (input)
+    PositionalEncoding parameters:
+        d_model: the size of the embedding vector (positional vector)
+        dropout: the dropout to be used on the sum of positional+embedding vector
+    """
+
+    def __init__(self):
+        super(TransformerTimeSeries, self).__init__()
+
+        self.feature_size = 12
+        self.dropout = 0.1
+        self.num_layers = 4
+        self.ts_length = 6
+
+        self.input_embedding = context_embedding(1, self.feature_size, 4)
+        self.positional_embedding = torch.nn.Embedding(80, self.feature_size) 
+
+        self.encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self.feature_size, nhead=2, dropout=self.dropout)
+        self.transformer_encoder = torch.nn.TransformerEncoder(self.encoder_layer, num_layers=self.num_layers)
+
+        self.decode_layer = torch.nn.TransformerEncoderLayer(d_model=self.feature_size, nhead=2)
+        self.transformer_decoder = torch.nn.TransformerEncoder(self.decode_layer, num_layers=self.num_layers)
+
+        self.fc1 = torch.nn.Linear(self.feature_size, 1)
+        self.f_class = torch.nn.Linear(self.feature_size, 1)
+        self.softmax = torch.nn.Softmax(dim=0)
+
+
+    def forward(self, x, y, attention_masks):
+        # concatenate observed points and time covariate
+        # (B*feature_size*n_time_points)
+        #z = torch.cat((y.unsqueeze(1), x.unsqueeze(1)), 1)
+        z = x
+        #print("z shape: ", z.shape)
+
+        # input_embedding returns shape (Batch size,embedding size,sequence len) -> need (sequence len,Batch size,embedding_size)
+        z_embedding = self.input_embedding(z).permute(2, 0, 1)
+        #print("z_embedding shape: ", z_embedding.shape)
+
+        # get my positional embeddings (Batch size, sequence_len, embedding_size) -> need (sequence len,Batch size,embedding_size)
+        positional_embeddings = self.positional_embedding(x.type(torch.long))
+        #print("positional_embeddings shape: ", positional_embeddings.shape)
+        positional_embeddings = positional_embeddings.reshape(positional_embeddings.shape[0], self.ts_length, -1).permute(1, 0, 2)
+        #print("positional_embeddings shape: ", positional_embeddings.shape)
+        input_embedding = z_embedding + positional_embeddings
+        #print("input_embedding shape: ", input_embedding.shape)
+        transformer_embedding = self.transformer_encoder(input_embedding, attention_masks)
+        #print("transformer_embedding shape: ", transformer_embedding.shape)
+
+        transformer_reconstruction = self.transformer_decoder(transformer_embedding, attention_masks)
+        #print("transformer_reconstruction shape: ", transformer_reconstruction.shape)
+        output = self.fc1(transformer_reconstruction.permute(1, 0, 2))
+        output = self.softmax(output) #to restrict to [0,1]
+        #print("output shape: ", output.shape)
+        psu_class = self.f_class(transformer_reconstruction.permute(1, 0, 2))
+        output = output.permute(0, 2, 1)
+        return output, psu_class, transformer_embedding, transformer_reconstruction
