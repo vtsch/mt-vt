323b0943ae854591dd90ca542e4bc8735fd23313
Vanessa Tschichold <vanessa@tschichold.com>
2022-08-13 17:18:10 +0200

    cleanup and add option for >2 clusters

--- clustering_algorithms.py
+++ clustering_algorithms.py
@@ -16,9 +16,13 @@ def kmeans(data, n_clusters, metric):
     centroids = kmeans.cluster_centers_
     return centroids, labels
 
-def run_kmeans(output, n_clusters, metric, name, experiment):
-    centroids, kmeans_labels = kmeans(output, n_clusters, metric)
-    plot_centroids(centroids, n_clusters, "%s kmeans centroids %s" %(metric, name), experiment)
+def run_kmeans(output, config, experiment):
+    centroids, kmeans_labels = kmeans(output, config.n_clusters, config.metric)
+    plot_centroids(centroids, config.n_clusters, "%s kmeans centroids %s" %(config.metric, config.experiment_name), experiment)
+
+    #if n_clusters > 2: summarize all >1 to 1 for clustering metrics
+    kmeans_labels[kmeans_labels >= 1] = 1
+
     return kmeans_labels
 
 def run_kmeans_only(data, n_clusters, metric):
