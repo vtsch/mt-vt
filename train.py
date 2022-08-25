ea8d7dacf514b4e8bb47f4d1a32612e0bd9f0d88
Vanessa Tschichold <vanessa@tschichold.com>
2022-08-25 16:11:26 +0200

    move training of ts-tcc to separate file

--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 import time
 import torch
-import os
-import torch.nn as nn
 from torch.optim import Adam
+import torch.nn.functional as F
 import pandas as pd
 from metrics import Meter
 from dataloader import get_dataloader, data_generator
 import numpy as np
 from models.transformer import generate_square_subsequent_mask
 from models.TC import TC
+from models.loss import NTXentLoss
 
 class Trainer:
     def __init__(self, config, experiment, data, net):
@@ -25,7 +25,7 @@ class Trainer:
         }
         self.attention_masks = generate_square_subsequent_mask(6)
         self.temporal_contr_model = TC(config).to(config.device)
-        self.temp_cont_optimizer = torch.optim.Adam(self.temporal_contr_model.parameters(), lr=config.lr, betas=(config.beta1, config.beta2), weight_decay=3e-4)
+        self.temp_cont_optimizer = Adam(self.temporal_contr_model.parameters(), lr=config.lr, betas=(config.beta1, config.beta2), weight_decay=3e-4)
         self.tstcc_train_dl, self.tstcc_valid_dl, self.tstcc_test_dl = data_generator(data, config)
     
     def _train_epoch(self, phase):
@@ -62,64 +62,21 @@ class Trainer:
         metrics = {k:v / i for k, v in metrics.items()}
         df_logs = pd.DataFrame([metrics])
 
-        return loss, df_logs    
-
-    def _train_tstcc_epoch(self):
-        meter = Meter()
-        meter.init_metrics('train')
-
-        self.net.train()
-        self.temporal_contr_model.train()
-
-        for batch_idx, (data, labels, aug1, aug2) in enumerate(self.tstcc_train_dl):
-            # send to device
-            data, labels = data.float().to(self.config.device), labels.long().to(self.config.device)
-            aug1, aug2 = aug1.float().to(self.config.device), aug2.float().to(self.config.device)
-
-            # optimizer
-            self.optimizer.zero_grad()
-            self.temp_cont_optimizer.zero_grad()
-
-            output = self.net(data)
-                
-            predictions, features = output
-            loss = self.criterion(predictions, labels)
-            loss.backward()
-            self.optimizer.step()
-            self.temp_cont_optimizer.step()
-
-            pred = predictions.max(1, keepdim=True)[1]
-            meter.update(pred.detach().numpy(), labels, 'train', loss.item())
-
-        metrics = meter.get_metrics()
-        metrics = {k:v / batch_idx for k, v in metrics.items()}
-        df_logs = pd.DataFrame([metrics])
-
-        return loss, df_logs
-        
+        return loss, df_logs            
     
     def run(self):
         for epoch in range(self.config.n_epochs):
             print('Epoch: %d | time: %s' %(epoch, time.strftime('%H:%M:%S')))
             
-            if self.config.MOD_TSTCC:
-                train_loss, train_logs = self._train_tstcc_epoch()
-                print(train_logs)
-                self.experiment.log_metrics(dic=train_logs, step=epoch)
-                val_loss, valid_acc, outs, trgs, features, val_logs = self.eval()
+            train_loss, train_logs = self._train_epoch(phase='train')
+            print(train_logs)
+            self.experiment.log_metrics(dic=train_logs, step=epoch)
+
+            with torch.no_grad():
+                val_loss, val_logs = self._train_epoch(phase='val')
                 print(val_logs)
                 self.experiment.log_metrics(dic=val_logs, step=epoch)
                 self.scheduler.step(val_loss)
-            else:
-                train_loss, train_logs = self._train_epoch(phase='train')
-                print(train_logs)
-                self.experiment.log_metrics(dic=train_logs, step=epoch)
-
-                with torch.no_grad():
-                    val_loss, val_logs = self._train_epoch(phase='val')
-                    print(val_logs)
-                    self.experiment.log_metrics(dic=val_logs, step=epoch)
-                    self.scheduler.step(val_loss)
             
             if (val_loss + 0.001) < self.best_loss:
                 self.best_loss = val_loss
@@ -132,61 +89,26 @@ class Trainer:
 
     def eval(self):
         self.net.eval()
-        self.temporal_contr_model.eval()
-
-        total_loss = []
-        total_acc = []
-        outs = np.array([])
         targets = np.array([])
         embeddings = np.array([])
 
         with torch.no_grad():
-            if self.config.MOD_TSTCC:
-                meter = Meter()
-                meter.init_metrics('val')
-                for batch_idx, (data, labels, _, _) in enumerate(self.tstcc_test_dl):
-                    data = data.float().to(self.config.device)
-                    labels = labels.long().to(self.config.device)
-                    output = self.net(data)
+            for i, (data, target, index) in enumerate(self.dataloaders['test']):
 
-                    # compute loss
-                    predictions, features = output
-                    loss = self.criterion(predictions, labels)
-                    total_acc.append(labels.eq(predictions.detach().argmax(dim=1)).float().mean())
-                    total_loss.append(loss.item())
-                    embeddings = np.append(embeddings, features.detach().numpy())
+                if self.config.MOD_TRANSFORMER == True:
+                    data = data.squeeze(1)
+                    index = index.squeeze(1)
+                    pred, psu_class, transf_emb, transf_reconst = self.net(index, data, self.attention_masks)
+                else:
+                    pred = self.net(data)
+                
+                embeddings = np.append(embeddings, pred.detach().numpy() )
+                targets = np.append(targets, target.detach().numpy())  #always +bs
+        
+            embeddings = embeddings.reshape(targets.shape[0], -1)
+            df_logs = pd.DataFrame([])
 
-                    pred = predictions.max(1, keepdim=True)[1]  # get the index of the max log-probability
-                    meter.update(pred.detach().numpy(), labels, 'val', loss.item())
-                    outs = np.append(outs, pred.cpu().numpy())
-                    targets = np.append(targets, labels.data.cpu().numpy())
-                    embeddings = np.append(embeddings, features.detach().numpy())
-
-                metrics = meter.get_metrics()
-                metrics = {k:v / batch_idx for k, v in metrics.items()}
-                df_logs = pd.DataFrame([metrics])
-
-                total_loss = torch.tensor(total_loss).mean()  # average loss
-                total_acc = torch.tensor(total_acc).mean()  # average acc
-                embeddings = embeddings.reshape(targets.shape[0], -1, features.shape[2])
-
-            else:
-                for i, (data, target, index) in enumerate(self.dataloaders['test']):
-
-                    if self.config.MOD_TRANSFORMER == True:
-                        data = data.squeeze(1)
-                        index = index.squeeze(1)
-                        pred, psu_class, transf_emb, transf_reconst = self.net(index, data, self.attention_masks)
-                    else:
-                        pred = self.net(data)
-                    
-                    embeddings = np.append(embeddings, pred.detach().numpy() )
-                    targets = np.append(targets, target.detach().numpy())  #always +bs
-            
-                embeddings = embeddings.reshape(targets.shape[0], -1)
-                df_logs = pd.DataFrame([])
-
-        return total_loss, total_acc, outs, targets, embeddings, df_logs
+        return targets, embeddings, df_logs
 
             
             
---
+++ traintstcc.py
@@ -0,0 +1,184 @@
+import os
+import sys
+
+sys.path.append("..")
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models.loss import NTXentLoss
+import torch
+from torch.optim import Adam
+import torch.nn.functional as F
+from metrics import Meter
+from dataloader import get_dataloader, data_generator
+import numpy as np
+from models.transformer import generate_square_subsequent_mask
+from models.TC import TC
+from models.loss import NTXentLoss
+import pandas as pd
+
+
+
+
+class TSTCCTrainer:
+    def __init__(self, config, experiment, data, net):
+        self.net = net.to(config.device)
+        self.config = config
+        self.experiment = experiment
+        self.criterion = config.loss_fn
+        self.optimizer = Adam(self.net.parameters(), lr=config.lr, betas=(config.beta1, config.beta2), weight_decay=3e-4)
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min')
+        self.best_loss = float('inf')
+        self.attention_masks = generate_square_subsequent_mask(6)
+        self.temporal_contr_model = TC(config).to(config.device)
+        self.temp_cont_optimizer = Adam(self.temporal_contr_model.parameters(), lr=config.lr, betas=(config.beta1, config.beta2), weight_decay=3e-4)
+        self.tstcc_train_dl, self.tstcc_valid_dl, self.tstcc_test_dl = data_generator(data, config)
+    
+    def run(self):
+        # Start training
+        for epoch in range(1, self.config.n_epochs + 1):
+            # Train and validate
+            train_loss, train_acc, train_logs = self.model_train()
+            self.experiment.log_metrics(dic=train_logs, step=epoch)
+            valid_loss, valid_acc, _, _, _ = self.model_evaluate()
+            if self.config.tstcc_training_mode != 'self_supervised':  # use scheduler in all other modes.
+                self.scheduler.step(valid_loss)
+
+            print(f'\nEpoch : {epoch}\n'
+                        f'Train Loss     : {train_loss:.4f}\t | \tTrain Accuracy     : {train_acc:2.4f}\n'
+                        f'Valid Loss     : {valid_loss:.4f}\t | \tValid Accuracy     : {valid_acc:2.4f}')
+
+        os.makedirs(self.config.model_save_path, exist_ok=True)
+        chkpoint = {'model_state_dict': self.net.state_dict(), 'temporal_contr_model_state_dict': self.temporal_contr_model.state_dict()}
+        torch.save(chkpoint, os.path.join(self.config.model_save_path, f'ckp_last.pt'))
+
+        if self.config.tstcc_training_mode != "self_supervised":  # no need to run the evaluation for self-supervised mode.
+            # evaluate on the test set
+            test_loss, test_acc, _, _ , _ = self.model_evaluate()
+            print(f'Test loss      :{test_loss:0.4f}\t | Test Accuracy      : {test_acc:0.4f}')
+
+
+    def model_train(self):
+        total_loss = []
+        total_acc = []
+        meter = Meter()
+        meter.init_metrics('train')
+
+        self.net.train()
+        self.temporal_contr_model.train()
+
+        for batch_idx, (data, labels, aug1, aug2) in enumerate(self.tstcc_train_dl):
+            # send to device
+            data, labels = data.float().to(self.config.device), labels.long().to(self.config.device)
+            aug1, aug2 = aug1.float().to(self.config.device), aug2.float().to(self.config.device)
+
+            # optimizer
+            self.optimizer.zero_grad()
+            self.temp_cont_optimizer.zero_grad()
+
+            if self.config.tstcc_training_mode == "self_supervised":
+                predictions1, features1 = self.net(aug1)
+                predictions2, features2 = self.net(aug2)
+
+                # normalize projection feature vectors
+                features1 = F.normalize(features1, dim=1)
+                features2 = F.normalize(features2, dim=1)
+
+                temp_cont_loss1, temp_cont_lstm_feat1 = self.temporal_contr_model(features1, features2)
+                temp_cont_loss2, temp_cont_lstm_feat2 = self.temporal_contr_model(features2, features1)
+
+                # normalize projection feature vectors
+                zis = temp_cont_lstm_feat1 
+                zjs = temp_cont_lstm_feat2 
+
+            else:
+                output = self.net(data)
+
+            # compute loss
+            if self.config.tstcc_training_mode == "self_supervised":
+                lambda1 = 1
+                lambda2 = 0.7
+                nt_xent_criterion = NTXentLoss(self.config.device, self.config.batch_size, self.config.temperature,
+                                            self.config.use_cosine_similarity)
+                loss = (temp_cont_loss1 + temp_cont_loss2) * lambda1 +  nt_xent_criterion(zis, zjs) * lambda2
+                
+            else: # supervised training or fine tuining
+                predictions, features = output
+                loss = self.criterion(predictions, labels)
+                total_acc.append(labels.eq(predictions.detach().argmax(dim=1)).float().mean())
+                #meter.update(pred.detach().numpy(), labels, 'train', loss.item())
+
+            total_loss.append(loss.item())
+            loss.backward()
+            self.optimizer.step()
+            self.temp_cont_optimizer.step()
+
+        metrics = meter.get_metrics()
+        metrics = {k:v / batch_idx for k, v in metrics.items()}
+        df_logs = pd.DataFrame([metrics])
+
+        total_loss = torch.tensor(total_loss).mean()
+
+        if self.config.tstcc_training_mode == "self_supervised":
+            total_acc = 0
+        else:
+            total_acc = torch.tensor(total_acc).mean()
+        return total_loss, total_acc, df_logs
+
+
+    def model_evaluate(self):
+        self.net.eval()
+        self.temporal_contr_model.eval()
+
+        total_loss = []
+        total_acc = []
+
+        criterion = nn.CrossEntropyLoss()
+        outs = np.array([])
+        trgs = np.array([])
+        embeddings = np.array([])
+
+        meter = Meter()
+        meter.init_metrics('val')
+
+        with torch.no_grad():
+            for batch_idx, (data, labels, _, _) in enumerate(self.tstcc_test_dl):
+                data, labels = data.float().to(self.config.device), labels.long().to(self.config.device)
+
+                if self.config.tstcc_training_mode == "self_supervised":
+                    pass
+                else:
+                    output = self.net(data)
+
+                # compute loss
+                if self.config.tstcc_training_mode != "self_supervised":
+                    predictions, features = output
+                    loss = self.criterion(predictions, labels)
+                    total_acc.append(labels.eq(predictions.detach().argmax(dim=1)).float().mean())
+                    total_loss.append(loss.item())
+
+                if self.config.tstcc_training_mode != "self_supervised":
+                    pred = predictions.max(1, keepdim=True)[1]  # get the index of the max log-probability
+                    outs = np.append(outs, pred.cpu().numpy())
+                    trgs = np.append(trgs, labels.data.cpu().numpy())
+                    embeddings = np.append(embeddings, features.detach().numpy())
+                
+                #meter.update(pred.detach().numpy(), labels, 'val', loss.item())
+
+        if self.config.tstcc_training_mode != "self_supervised":
+            total_loss = torch.tensor(total_loss).mean()  # average loss
+            metrics = meter.get_metrics()
+            metrics = {k:v / batch_idx for k, v in metrics.items()}
+            df_logs = pd.DataFrame([metrics])
+            embeddings = embeddings.reshape(trgs.shape[0], -1, features.shape[2])
+        else:
+            total_loss = 0
+        if self.config.tstcc_training_mode == "self_supervised":
+            total_acc = 0
+            return total_loss, total_acc, [], [], []
+        else:
+            total_acc = torch.tensor(total_acc).mean()  # average acc
+        return total_loss, total_acc, outs, trgs, embeddings
