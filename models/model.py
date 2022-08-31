8d77ab4f53e49489520fc8c23354c3f67963c1c8
Vanessa Tschichold <vanessa@tschichold.com>
2022-08-31 15:51:31 +0200

    update model for all 3 conv blocks

--- models/model.py
+++ models/model.py
@@ -1,59 +1,45 @@
 from torch import nn
 
 class base_Model(nn.Module):
-    def __init__(self, configs):
+    def __init__(self, config):
         super(base_Model, self).__init__()
         
-        """
         self.conv_block1 = nn.Sequential(
-            nn.Conv1d(configs.input_channels, 32, kernel_size=configs.kernel_size,
-                      stride=configs.stride, bias=False, padding=(configs.kernel_size//2)),
-            nn.BatchNorm1d(32),
+            nn.Conv1d(config.input_channels, 4, kernel_size=8, stride=1, bias=False, padding=4),
+            nn.BatchNorm1d(4),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=2, stride=2, padding=1),
-            nn.Dropout(configs.dropout)
+            nn.Dropout(config.dropout)
         )
 
         self.conv_block2 = nn.Sequential(
-            nn.Conv1d(32, 64, kernel_size=8, stride=1, bias=False, padding=4),
-            nn.BatchNorm1d(64),
+            nn.Conv1d(4, 8, kernel_size=8, stride=1, bias=False, padding=4),
+            nn.BatchNorm1d(8),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=2, stride=2, padding=1)
         )
 
         self.conv_block3 = nn.Sequential(
-            nn.Conv1d(64, configs.final_out_channels, kernel_size=8, stride=1, bias=False, padding=4),
-            nn.BatchNorm1d(configs.final_out_channels),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=1),
-        )
-        """
-        self.conv_block1 = nn.Sequential(
-            nn.Conv1d(configs.input_channels, 16, kernel_size=configs.kernel_size,
-                      stride=configs.stride, bias=False, padding=(configs.kernel_size//2)),
-            nn.BatchNorm1d(16),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2, padding=1),
-            nn.Dropout(configs.dropout)
-        )
-
-        self.conv_block2 = nn.Sequential(
-            nn.Conv1d(16, configs.final_out_channels, kernel_size=2, stride=1, bias=False, padding=4),
-            nn.BatchNorm1d(configs.final_out_channels),
+            nn.Conv1d(8, config.final_out_channels, kernel_size=2, stride=1, bias=False, padding=4),
+            nn.BatchNorm1d(config.final_out_channels),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=2, stride=2, padding=1)
         )
 
-        model_output_dim = configs.emb_size
-        self.logits = nn.Linear(model_output_dim * configs.final_out_channels, configs.n_clusters)
+        model_output_dim = config.emb_size
+        self.logits = nn.Linear(model_output_dim * config.final_out_channels, config.n_clusters)
 
     def forward(self, x_in):
-        x_in = x_in.reshape(x_in.shape[0], 1, -1)
+        # x_in shape: (batch_size, time_steps)
+        x_in = x_in.reshape(-1, 1, x_in.shape[1])
         x = self.conv_block1(x_in)
         x = self.conv_block2(x)
-        #x = self.conv_block3(x)
+        x = self.conv_block3(x)
 
         x_flat = x.reshape(x.shape[0], -1)
         logits = self.logits(x_flat)
-        # x are (batch_size, #channels, seq_len)
         return logits, x
+
+        #logits shape: (batch_size, n_clusters)
+        #x shape: (batch_size, final_out_channels, time_steps)
+
