611fc405b40e6a601f387d03c37e4fcc86ec5762
Vanessa Tschichold <vanessa@tschichold.com>
2022-09-06 09:15:51 +0200

    fix plots

--- kmeans.py
+++ kmeans.py
@@ -5,9 +5,8 @@ import numpy as np
 
 def plot_datapoints_of_cluster(data, labels, cluster_id, label_color_map, title, experiment):
     datapoints = data[labels == cluster_id]
-    print("Cluster %d has %d datapoints" %(cluster_id, datapoints.shape[0]))
-    print(datapoints.shape)
-    
+    print("Cluster %d has %d datapoints predicted" %(cluster_id, datapoints.shape[0]))
+
     fig1 = plt.figure("%s cluster %d" %(title, cluster_id))
     #plot data points with label = cluster_id
     for i in range(datapoints.shape[0]):
@@ -38,10 +37,9 @@ def plot_datapoints(data, labels, title, experiment):
     plt.legend(['%d' %i for i in range(np.unique(labels).shape[0])], loc='upper left', title="Clusters")
     experiment.log_figure(figure=fig0, figure_name="datapoints_%s" %title)
 
-    plot_datapoints_of_cluster(data, labels, 0, label_color_map, title, experiment)
-    plot_datapoints_of_cluster(data, labels, 1, label_color_map, title, experiment)
-    #boxplot_datapoints_of_cluster(data, labels, 0, label_color_map, title, experiment)
-    #boxplot_datapoints_of_cluster(data, labels, 1, label_color_map, title, experiment)
+    for i in range(np.unique(labels).shape[0]):
+        plot_datapoints_of_cluster(data, labels, i, label_color_map, title, experiment)
+
 
 def plot_centroids(centroids, n_clusters, title, experiment):
     for i in range(n_clusters):
@@ -58,13 +56,19 @@ def kmeans(data, n_clusters, metric):
     centroids = kmeans.cluster_centers_
     return centroids, labels
 
-def run_kmeans(output, config, experiment):
+def run_kmeans_and_plots(output, config, experiment):
     centroids, kmeans_labels = kmeans(output, config.n_clusters, config.metric)
     plot_centroids(centroids, config.n_clusters, "%s kmeans centroids %s" %(config.metric, config.experiment_name), experiment)
+    plot_datapoints(output, kmeans_labels, config.experiment_name, experiment)
     return kmeans_labels
 
 def run_kmeans_only(data, config):
     data = data.reshape(data.shape[0], data.shape[1], 1)
     kmeans = TimeSeriesKMeans(n_clusters=config.n_clusters, metric=config.metric, max_iter=20, random_state=0, n_init=5).fit(data)
+    kmeans_labels = kmeans.predict(data)
+    return kmeans_labels
+
+def run_sklearn_kmeans(data, config):
+    kmeans = cluster.KMeans(n_clusters=config.n_clusters, random_state=42).fit(data)
     kmeans_labels = kmeans.predict(data)
     return kmeans_labels
