59be8633c6213ddab015a19ffa4a3498513440ed
Vanessa Tschichold <vanessa@tschichold.com>
2022-09-19 14:34:57 +0200

    change baseline models to correct input shape for context
    change shape of context to (ts_length + context_dim)

--- models/baseline_models.py
+++ models/baseline_models.py
@@ -9,23 +9,24 @@ import matplotlib.pyplot as plt
 class SimpleAutoencoder(nn.Module):
     def __init__(self, config):
         '''
-        Simple Autoencoder, input is (batch_size, seq_length, feat_dim)
+        Simple Autoencoder, input is (batch_size, seq_length+nr_features)
         '''
         super(SimpleAutoencoder, self).__init__()
-        self.fc = nn.Linear(in_features=config.feat_dim*config.ts_length, out_features=config.emb_size)
+        self.fc = nn.Linear(in_features=config.feat_dim, out_features=config.emb_size)
 
     def forward(self, x):
-        x = x.view(x.shape[0], -1) # (batch_size, seq_length * feat_dim)
+        x = x.view(x.shape[0], -1) # (batch_size, feat_dim)
         x = self.fc(x)
         return x
 
 class DeepAutoencoder(nn.Module):
     def __init__(self, config):
         super(DeepAutoencoder, self).__init__()
-        self.fc1 = nn.Linear(in_features=config.feat_dim*config.ts_length, out_features=240)
+        self.fc1 = nn.Linear(in_features=config.feat_dim, out_features=240)
         self.fc2 = nn.Linear(in_features=240, out_features=120)
         self.fc3 = nn.Linear(in_features=120, out_features=60)
         self.fc4 = nn.Linear(in_features=60, out_features=config.emb_size)
+        self.avgpool = nn.AvgPool1d(kernel_size=2)
 
     def forward(self, x):
         x = x.view(x.shape[0], -1)
@@ -33,6 +34,7 @@ class DeepAutoencoder(nn.Module):
         x = self.fc2(x)
         x = self.fc3(x)
         x = self.fc4(x)
+        x = self.avgpool(x)
         return x
 
 
@@ -83,7 +85,7 @@ class CNN(nn.Module):
         super().__init__()
         self.config = config
         self.conv1 = ConvNormPool(
-            input_size=config.feat_dim,
+            input_size=1,
             hidden_size=config.bl_hidden_size,
             kernel_size=config.kernel_size,
         )
@@ -96,10 +98,7 @@ class CNN(nn.Module):
         self.fc = nn.Linear(in_features=config.bl_hidden_size//2, out_features=config.emb_size)
         
     def forward(self, input):
-        if not self.config.context:
-            input = input.reshape(input.shape[0], 1, input.shape[1])
-        else:
-            input = input.permute(0, 2, 1)
+        input = input.reshape(input.shape[0], 1, input.shape[1])
         x = self.conv1(input)
         x = self.conv2(x)
         x = self.avgpool(x)        
@@ -133,11 +132,8 @@ class LSTMencoder(nn.Module):
         :                              hidden gives the hidden state and cell state for the last
         :                              element in the sequence 
         '''
-        if not self.config.context:
-            x = x.reshape(x.shape[0], x.shape[1], 1)
         lstm_out, self.hidden = self.lstm(x)
         emb = self.avgpool(lstm_out)
         emb = self.fc(emb)
-        emb = emb.reshape(emb.shape[0], emb.shape[1] * emb.shape[2])
         return emb     
     
