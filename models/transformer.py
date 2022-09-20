c62b280cc103d3787c48457c043c04253fd15dee
Vanessa Tschichold <vanessa@tschichold.com>
2022-09-20 18:18:10 +0200

    move pos enc to new file

--- models/transformer.py
+++ models/transformer.py
@@ -1,5 +1,4 @@
 import torch
-import torch.optim as optim
 import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
@@ -7,7 +6,7 @@ from typing import Optional, Any
 import math
 from torch import long, nn, Tensor
 from torch.nn.modules import MultiheadAttention, Linear, Dropout, BatchNorm1d, TransformerEncoderLayer
-
+from pos_enc import positional_encoding
 
 #https://github.com/sunnyqiny/Unsupervised-Temporal-Embedding-and-Clustering/tree/cc5a41df905efbac11788a43b6151c08c68b8c6c 
 
@@ -28,76 +27,12 @@ def generate_square_subsequent_mask(config):
 
 # from https://github.com/gzerveas/mvts_transformer 
 
-
 def _get_activation_fn(activation):
     if activation == "relu":
         return F.relu
     elif activation == "gelu":
         return F.gelu
     raise ValueError("activation should be relu/gelu, not {}".format(activation))
-
-
-class FixedPositionalEncoding(nn.Module):
-    r"""Inject some information about the relative or absolute position of the tokens
-        in the sequence. The positional encodings have the same dimension as
-        the embeddings, so that the two can be summed. Here, we use sine and cosine
-        functions of different frequencies.
-    .. math::
-        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
-        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
-        \text{where pos is the word position and i is the embed idx)
-    Args:
-        d_model: the embed dim (required).
-        dropout: the dropout value (default=0.1).
-        max_len: the max. length of the incoming sequence (default=1024).
-    """
-    def __init__(self, d_model, dropout=0.1, max_len=1024, scale_factor=1.0):
-        super(FixedPositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-        self.max_len = max_len
-        self.d_model = d_model
-
-    def forward(self, x):
-        r"""Inputs of forward function
-        Args:
-            x: the sequence fed to the positional encoder model (required).
-        Shape:
-            x: [sequence length, batch size, embed dim]
-            output: [sequence length, batch size, embed dim]
-        """
-        pe = torch.zeros(10, self.d_model)  # positional encoding
-        position = torch.arange(0, 10, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * (-math.log(10000.0) / self.d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-
-        x = x + pe[:x.size(0), :]
-        return self.dropout(x)
-
-class LearnablePositionalEncoding(nn.Module):
-
-    def __init__(self, config):
-        super(LearnablePositionalEncoding, self).__init__()
-        self.config = config
-        self.dropout = nn.Dropout(p=config.dropout)
-        # Each position gets its own embedding
-        # Since indices are always 0 ... max_len, we don't have to do a look-up
-        self.pe = nn.Parameter(torch.empty(10, 1, config.d_model))  # requires_grad automatically set to True
-        nn.init.uniform_(self.pe, -0.02, 0.02)
-
-    def forward(self, x):
-        r"""Inputs of forward function
-        Args:
-            x: the sequence fed to the positional encoder model (required).
-        Shape:
-            x: [sequence length, batch size, embed dim]
-            output: [sequence length, batch size, embed dim]
-        """
-
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
-
 
 class TransformerBatchNormEncoderLayer(nn.modules.Module):
     r"""This transformer encoder layer block is made up of self-attn and feedforward network.
@@ -158,18 +93,14 @@ class TransformerBatchNormEncoderLayer(nn.modules.Module):
 
 class TSTransformerEncoder(nn.Module):
 
-    def __init__(self, config, activation='gelu', norm='BatchNorm'):
+    def __init__(self, config, activation='gelu'):
         super(TSTransformerEncoder, self).__init__()
 
         self.config = config
 
         self.project_inp = nn.Linear(1, self.config.d_model)
-        self.pos_enc = LearnablePositionalEncoding(config)
 
-        if norm == 'LayerNorm':
-            encoder_layer = TransformerEncoderLayer(self.config.d_model, self.config.n_heads, self.config.dim_feedforward, dropout=self.config.dropout, activation=activation)
-        else:
-            encoder_layer = TransformerBatchNormEncoderLayer(self.config.d_model, self.config.n_heads, self.config.dim_feedforward, dropout=self.config.dropout, activation=activation)
+        encoder_layer = TransformerBatchNormEncoderLayer(self.config.d_model, self.config.n_heads, self.config.dim_feedforward, dropout=self.config.dropout, activation=activation) #use batch normalization
 
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, self.config.num_layers)
 
@@ -180,9 +111,7 @@ class TSTransformerEncoder(nn.Module):
         self.dropout = nn.Dropout(self.config.dropout)
         self.max_pool = nn.MaxPool1d(kernel_size=self.config.ts_length)
 
-        self.feat_dim = self.config.feat_dim
-
-    def forward(self, indices, data, context, attention_masks):
+    def forward(self, indices, data, attention_masks):
         """
         Args:
             X: (batch_size, seq_length, feat_dim) torch tensor of masked features (input)
@@ -197,17 +126,10 @@ class TSTransformerEncoder(nn.Module):
         indices = indices.permute(1, 0, 2)
 
         inp = self.project_inp(inp) * math.sqrt(self.config.d_model)  # [seq_length, batch_size, d_model] project input vectors to d_model dimensional space
-        if self.config.learnable_pos_enc:
-            inp = self.pos_enc(inp)
-        else:
-            #print("inp", inp)
-            #print("indices", indices) 
-            inp = inp + indices
-            print("inp after add", inp)
-            inp = self.dropout(inp)
+        pos_enc_inp = positional_encoding(self.config, inp, indices)  # add positional encoding
         
         # NOTE: logic for padding masks is reversed to comply with definition in MultiHeadAttention, TransformerEncoderLayer
-        output = self.transformer_encoder(inp, src_key_padding_mask=~attention_masks)  # (seq_length, batch_size, d_model)
+        output = self.transformer_encoder(pos_enc_inp, src_key_padding_mask=~attention_masks)  # (seq_length, batch_size, d_model)
         output = self.act(output)  # the output transformer encoder/decoder embeddings don't include non-linearity
         output = output.permute(1, 0, 2)  # (batch_size, seq_length, d_model)
         output = self.dropout(output)
