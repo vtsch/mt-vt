686c848b09785d193158e267e3479c9df8b42fde
Vanessa Tschichold <vanessa@tschichold.com>
2022-09-29 19:20:22 +0200

    remove pos enc

--- pos_enc.py
+++ pos_enc.py
@@ -6,15 +6,13 @@ import numpy as np
 from typing import Optional, Any
 import math
 from torch import long, nn, Tensor
-from rotary_embedding_torch import RotaryEmbedding, apply_rotary_emb
+#from rotary_embedding_torch import RotaryEmbedding, apply_rotary_emb
 
 def positional_encoding(config, inp, indices):
     dropout = nn.Dropout(p=config.dropout)
     lpe = nn.Parameter(torch.empty(config.batch_size, config.emb_size))  # requires_grad automatically set to True
     nn.init.uniform_(lpe, -0.02, 0.02)
-    rope = RotaryEmbedding(dim=config.ts_length)
-    #print("indices.shape", indices.shape)
-    #print("inp.shape", inp.shape)
+    #rope = RotaryEmbedding(dim=config.ts_length)
 
     if config.pos_enc == 'none':
         out = inp
@@ -32,10 +30,12 @@ def positional_encoding(config, inp, indices):
         inp = inp + lpe[:inp.size(0), :]
         out = dropout(inp)
 
+    """
     if config.pos_enc == "rotary_pos_enc":
         print("Using rotary positional encoding")
         print("inp shape", inp)
         out = rope(inp)
         print(out)
         out = dropout(out)
+    """
     return out
