bbdf62937941e22c38d1f36febff1878e41985c7
Vanessa Tschichold <vanessa@tschichold.com>
2022-10-01 18:32:14 +0200

    update preprocess for furst
    - categorize risk labels
    - add column for cancer
    - choose new date for psa test

--- preprocess_furst.py
+++ preprocess_furst.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 
 def reshape_psa_data_and_save():
-    file_name = "data/psadata_furst_raw.csv"
+    file_name = "data/psadata_furst_measurements.csv"
     df = pd.read_csv(file_name, header=0)
 
     # remove time from date
-    df['sampling_date'] = df['sampling_date'].map(lambda x: x.split()[0])
+    df['ambiguous_date'] = df['ambiguous_date'].map(lambda x: x.split()[0])
     # print(df1.head(5))
 
     # merge date and measurement
-    df['datapsa'] = df['sampling_date'] + [' '] + df['result_numeric'].astype(str)
+    df['datapsa'] = df['ambiguous_date'] + [' '] + df['result_numeric'].astype(str)
     #print(df1.head(5))
 
     # put all measurements and dates in columns per patient
@@ -31,10 +31,10 @@ def reshape_psa_data_and_save():
             del df1['datapsa_' + str(i)]
     #print(df1.head(5))
 
-    df1.to_csv("/tsd/p594/home/p594-vanessat/mt-vt/data/psadata_furst_measurements.csv", index=False)
+    df1.to_csv("/tsd/p594/home/p594-vanessat/mt-vt/data/psadata_furst_measurements_restructured.csv", index=False)
 
 def get_measurements():
-    file_name = "data/psadata_furst_measurements.csv"
+    file_name = "data/psadata_furst_measurements_restructured.csv"
     df = pd.read_csv(file_name, header=0)
     # drop rows with less than 9 entries --> need to have more than 4 measurements
     df = df.dropna(axis=0, thresh=9)
@@ -51,21 +51,38 @@ def get_ages():
 def get_labels():
     file_name = "data/psadata_furst_labels.csv"
     df = pd.read_csv(file_name, header=0)
-    print("nr of rows labels: ", len(df)) #155'865
+    print("nr of rows labels: ", len(df)) #155'865 --> without missing: 118'698
+    # delete rows with 'missing' in npcc_risk_class_group_1 as missing = couldn't make diagnosis
+    df = df[df.npcc_risk_class_group_1 != 'Missing']
+    # encode risk classes as 1, 2, 3
+    df['npcc_risk_class_group_1'] = df['npcc_risk_class_group_1'].map(lambda x: 1 if x == 'Localized' else x)
+    df['npcc_risk_class_group_1'] = df['npcc_risk_class_group_1'].map(lambda x: 2 if x == 'Advanced' else x)
+    df['npcc_risk_class_group_1'] = df['npcc_risk_class_group_1'].map(lambda x: 3 if x == 'Metastatic' else x)
+    print("nr of rows labels without missing: ", len(df)) #155'865 --> without missing: 118'698
     return df
 
 
 if __name__ == "__main__":
-    # reshape_psa_data_and_save()
+    reshape_psa_data_and_save()
     df_m = get_measurements()
     df_a = get_ages()
     df_l = get_labels()
 
     # merge dataframe psa levels and ages on id
-    df = pd.merge(df_m, df_a, on='ss_number_id')
+    df = pd.merge(df_m, df_a, on='ss_number_id', how='left')
     print("nr of rows after merge psa and age: ", len(df)) # 256'834
     df = pd.merge(df, df_l, on='ss_number_id', how='left')
-    print("nr of rows after merge labels: ", len(df))
+    print("nr of rows after merge labels: ", len(df)) # 118'698
+    # fill 0 for all patients without label
+    df['npcc_risk_class_group_1'] = df['npcc_risk_class_group_1'].fillna(0)
+    df['npcc_risk_class_group_2'] = df['npcc_risk_class_group_2'].fillna(0)
+    df['npcc_risk_class_group_3'] = df['npcc_risk_class_group_3'].fillna(0)
+    # create row cancer, add 0 if npcc_risk_class_group_1 == 0, else 1
+    df['cancer'] = np.where(df['npcc_risk_class_group_1'] == 0, 0, 1)
+    #print nr of patients with cancer
+    print("nr of patients with cancer: ", len(df[df['cancer'] == 1])) # 118'698
+    print("nr of patients without cancer: ", len(df[df['cancer'] == 0])) # rest
+
     print(df.head(5))
 
     # save the dataframe
