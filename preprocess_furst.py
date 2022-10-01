1657fa0c717c35bad3496a95fbc5b3e6e7918717
Vanessa Tschichold <vanessa@tschichold.com>
2022-10-01 14:06:45 +0200

    add typing and renaming

--- preprocess_furst.py
+++ preprocess_furst.py
@@ -18,7 +18,7 @@ def reshape_psa_data_and_save():
             .unstack(fill_value='')
             .add_prefix('datapsa_')
             .reset_index())
-    print(df1.head(5))
+    #print(df1.head(5))
 
     # delete all columns after column 21
     df1 = df1.iloc[:, :22]
@@ -29,36 +29,43 @@ def reshape_psa_data_and_save():
             df1['psa_' + str(i)] = [x[10:] for x in df1['datapsa_' + str(i)]]
             #delete the old column
             del df1['datapsa_' + str(i)]
-    print(df1.head(5))
+    #print(df1.head(5))
 
     df1.to_csv("/tsd/p594/home/p594-vanessat/mt-vt/data/psadata_furst_measurements.csv", index=False)
 
 def get_measurements():
     file_name = "data/psadata_furst_measurements.csv"
     df = pd.read_csv(file_name, header=0)
+    # drop rows with less than 9 entries --> need to have more than 4 measurements
+    df = df.dropna(axis=0, thresh=9)
+    print("nr of rows psa data: ", len(df)) # 912'111 --> with threshold 514'002
     return df
 
 def get_ages():
     file_name = "data/psadata_furst_age.csv"
     df = pd.read_csv(file_name, header=0)
     df['date_of_birth_15'] = df['date_of_birth_15'].map(lambda x: x.split()[0])
+    print("nr of rows ages: ", len(df)) # 1'879'007
     return df
 
 def get_labels():
     file_name = "data/psadata_furst_labels.csv"
     df = pd.read_csv(file_name, header=0)
+    print("nr of rows labels: ", len(df)) #155'865
     return df
 
 
 if __name__ == "__main__":
-    reshape_psa_data_and_save()
+    # reshape_psa_data_and_save()
     df_m = get_measurements()
     df_a = get_ages()
     df_l = get_labels()
 
-    # merge all dataframes and join on ss_number_id
+    # merge dataframe psa levels and ages on id
     df = pd.merge(df_m, df_a, on='ss_number_id')
-    df = pd.merge(df, df_l, on='ss_number_id')
+    print("nr of rows after merge psa and age: ", len(df)) # 256'834
+    df = pd.merge(df, df_l, on='ss_number_id', how='left')
+    print("nr of rows after merge labels: ", len(df))
     print(df.head(5))
 
     # save the dataframe
