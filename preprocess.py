3237f4df44b149fa43f7e33c711bdb2866c3a7af
Vanessa Tschichold <vanessa@tschichold.com>
2022-10-02 15:02:18 +0200

    add dataloading and positional encodings for furst dataset

--- preprocess.py
+++ preprocess.py
@@ -43,26 +43,14 @@ def load_plco_df(df: pd.DataFrame) -> pd.DataFrame:
 
 def load_furst_df(file_name: str) -> pd.DataFrame:
     df = pd.read_csv(file_name, header=0)
-    # select columns with psa data, set threshold to have at least 5 measurements
-    # ss_nr_id =0
+    # ss_nr_id: 0
     # psa levels: 2, 4, 6, 8, 10, 12, ... 42
-    # label: 44
-    # in column 44, change all "localized" to 1, "advanced" to 2, "metastatic" to 3, "missing"  to 0 in column
-    df.replace(to_replace="localized", value=1, inplace=True)
-    df.replace(to_replace="advanced", value=2, inplace=True)
-    df.replace(to_replace="metastatic", value=3, inplace=True)
-    df.replace(to_replace="missing", value=0, inplace=True)
-
-    df = df.iloc[:, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44]]
-    #df.dropna(thresh=4, inplace=True)
-    if df.iloc[:, :8].isnull().values.any():
-        df.dropna(axis=0, inplace=True)
-        
-    print("class distribution: \n", df['npcc_risk_class_group_1'].value_counts())
-    df.fillna(0, inplace=True)
-    print(df.head(10))
+    # psa days: 1, 5, 7, 9, 11, 13, ... 41
+    # date_of_birth: 43
+    # npcc_risk_class_group_1-3: 44, 45, 46
+    # cancer: 47
+    df = df.iloc[:, [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 47, 0]]
     return df
-
 
 def create_context_df(df: pd.DataFrame, config: Bunch) -> pd.DataFrame:
     # select columns with demographic data
@@ -89,49 +77,81 @@ def create_context_df(df: pd.DataFrame, config: Bunch) -> pd.DataFrame:
     df = df.iloc[:, indices]
     return df
 
-def load_psa_and_absolutedays_df(df: pd.DataFrame) -> pd.DataFrame:
-    # psa_levels per year: 69-74
-    # day of psa level mesaurements: 80-85
-    # pros_cancer label: 4
-    df = df.iloc[:, [69, 70, 71, 72, 73, 74, 80, 81, 82, 83, 84, 85, 4, 44]]
+def load_psa_and_absolutedays_df(df: pd.DataFrame, config: Bunch) -> pd.DataFrame:
+    if config.dataset == "plco":
+        # psa_levels per year: 69-74
+        # day of psa level mesaurements: 80-85
+        # pros_cancer label: 4
+        df = df.iloc[:, [69, 70, 71, 72, 73, 74, 80, 81, 82, 83, 84, 85, 4, 44]]
+    elif config.dataset == "furst":
+        # psa levels: 2, 4, 6, 8, 10, 12, ... 42
+        # psa days: 1, 3, 5, 7, 9, 11, 13, ... 41
+        # cancer label: 47 
+        df = df.iloc[:, [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 47, 0]]
+    else:
+        raise ValueError("Dataset not supported")
     return df
 
-def load_psa_and_deltadays_df(df: pd.DataFrame) -> pd.DataFrame:
+def load_psa_and_deltadays_df(df: pd.DataFrame, config: Bunch) -> pd.DataFrame:
     '''
     Parameters:
         df (pd.DataFrame): raw psa dataframe 
     Returns:
         df (pd.DataFrame): dataframe with psa levels and delta days (days between psa measurements)
     '''
-    # psa_levels per year: 69-74
-    # day of psa level mesaurements: 80-85
-    # pros_cancer label: 4
-    df = df.iloc[:, [69, 70, 71, 72, 73, 74, 80, 81, 82, 83, 84, 85, 4, 44]]
-    #calculate deltatime between psa measurements
-    df['psa_delta0'] = 0
-    for i in range(1, 6):
-        df['psa_delta' + str(i)] = df['psa_days' + str(i)] - df['psa_days' + str(i-1)]
+    # calculate deltatime between psa measurements and add to dataframe and delete measurement days
+    if config.dataset == "plco":
+        # psa_levels per year: 69-74
+        # day of psa level mesaurements: 80-85
+        # pros_cancer label: 4
+        df = df.iloc[:, [69, 70, 71, 72, 73, 74, 80, 81, 82, 83, 84, 85, 4, 44]]
+        
+        df['psa_delta0'] = 0
+        for i in range(1, 6):
+            df['psa_delta' + str(i)] = df['psa_days' + str(i)] - df['psa_days' + str(i-1)]
+        df.drop(['psa_days0', 'psa_days1', 'psa_days2', 'psa_days3', 'psa_days4', 'psa_days5'], axis=1, inplace=True)
+        print(df.head(5))
 
-    df.drop(['psa_days0', 'psa_days1', 'psa_days2', 'psa_days3', 'psa_days4', 'psa_days5'], axis=1, inplace=True)
+    elif config.dataset == "furst":
+        df['psa_delta0'] = 0
+        for i in range(1, 20):
+            df['psa_delta' + str(i)] = (df['date_' + str(i)] - df['date_' + str(i-1)]) / np.timedelta64(1, 'D')
+        for i in range(20):
+            df.drop(['date_' + str(i)], axis=1, inplace=True)
+    else:
+        raise ValueError("Dataset not supported")
+    
     return df
 
-def load_psa_and_age_df(df: pd.DataFrame) -> pd.DataFrame:
+def load_psa_and_age_df(df: pd.DataFrame, config: Bunch) -> pd.DataFrame:
     '''
     Parameters:
         df (pd.DataFrame): raw psa dataframe 
     Returns:
         df (pd.DataFrame): dataframe with psa levels and age at trial date
     '''
-    # psa_levels per year: 69-74
-    # age at trial entry: 205
-    # pros_cancer label: 4
-    df = df.iloc[:, [69, 70, 71, 72, 73, 74, 205, 4, 44]]
-    # calculate age at psa measurement
-    df['psa_age0'] = df['age']
-    for i in range(1, 6):
-        df['psa_age' + str(i)] = df['age'] + i
-    
-    df.drop(['age'], axis=1, inplace=True)
+    if config.dataset == "plco":
+        # psa_levels per year: 69-74
+        # age at trial entry: 205
+        # pros_cancer label: 4
+        df = df.iloc[:, [69, 70, 71, 72, 73, 74, 205, 4, 44]]
+        # calculate age at psa measurement
+        df['psa_age0'] = df['age']
+        for i in range(1, 6):
+            df['psa_age' + str(i)] = df['age'] + i
+        
+        df.drop(['age'], axis=1, inplace=True)
+
+    elif config.dataset == "furst":
+        for i in range(0, 20):
+            df['psa_age' + str(i)] = (df['date_' + str(i)] - df['date_of_birth_15']) / np.timedelta64(1, 'Y')
+            print(df['psa_age' + str(i)])
+        for i in range(20):
+            df.drop(['date_' + str(i)], axis=1, inplace=True)
+        df.drop(['date_of_birth_15'], axis=1, inplace=True)
+    else:
+        raise ValueError("Dataset not supported")
+
     return df
 
 def load_psa_data_to_pd(file_name: str, config: dict) -> pd.DataFrame:
@@ -144,35 +164,60 @@ def load_psa_data_to_pd(file_name: str, config: dict) -> pd.DataFrame:
     '''
     df_raw = pd.read_csv(file_name, header=0)
 
+    # convert dates to datetime in furst dataset
+    if config.dataset == 'furst':
+        for i in range(0, 20):
+            df_raw['date_' + str(i)] = df_raw['date_' + str(i)].str.replace('-', '')
+            df_raw['date_' + str(i)] = pd.to_datetime(df_raw['date_' + str(i)], format='%Y-%m-%d')
+        df_raw['date_of_birth_15'] = pd.to_datetime(df_raw['date_of_birth_15'], format='%Y-%m-%d')
+
+    # load positional encodings
     if config.pos_enc == "absolute_days":
-        df = load_psa_and_absolutedays_df(df_raw)
+        df = load_psa_and_absolutedays_df(df_raw, config)
     elif config.pos_enc == "delta_days":
-        df = load_psa_and_deltadays_df(df_raw)
+        df = load_psa_and_deltadays_df(df_raw, config)
     elif config.pos_enc == "age_pos_enc":
-        df = load_psa_and_age_df(df_raw)
+        df = load_psa_and_age_df(df_raw, config)
     else:
-        df = load_plco_df(df_raw)
+        if config.dataset == "plco":
+            df = load_plco_df(df_raw)
+        elif config.dataset == "furst":
+            df = load_furst_df(df_raw)
+        else:
+            raise ValueError("Dataset not supported")
     
-    if config.context:
+    # load context 
+    if config.context and config.dataset == "plco":
         df_context = create_context_df(df_raw, config)
         df = pd.merge(df, df_context, on='plco_id', how='inner')
+    else:
+        print("No context data available")
 
     if config.upsample:
         df = upsample_data(df, config)
-    
-    # drop rows that have nan in psa levels
-    if df.iloc[:, :6].isnull().values.any():
-        df.dropna(axis=0, inplace=True)
 
-    print("class distribution: \n", df['pros_cancer'].value_counts())
+    # print class distribution and final cleanup for models
+    if config.dataset == "plco":
+        # drop rows that have nan in psa levels
+        if df.iloc[:, :6].isnull().values.any():
+            df.dropna(axis=0, inplace=True)
 
-    df.drop(['plco_id'], axis=1, inplace=True)
-    #fill nan values with -1
-    df.fillna(-1, inplace=True)
-    #df[df < 0] = 0
+        print("class distribution: \n", df['pros_cancer'].value_counts())
+
+        df.drop(['plco_id'], axis=1, inplace=True)
+        #fill nan values with -1
+        df.fillna(-1, inplace=True)
+
+    elif config.dataset == "furst":
+        print("class distribution: \n", df['cancer'].value_counts())
+
+        df.drop(['ss_number_id'], axis=1, inplace=True)
+        #fill nan values with -1
+        df.fillna(-1, inplace=True)
+
+        print(df.head(10))
+    else:
+        raise ValueError("Dataset not supported")
 
     return df
-
-    
-
 
