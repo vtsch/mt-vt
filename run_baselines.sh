1f71e2ccbac009cd4ee0648dc6d7383cc464b889
Vanessa Tschichold <vanessa@tschichold.com>
2022-10-02 22:00:42 +0200

    make run baselines complete

--- run_baselines.sh
+++ run_baselines.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
+python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 3 -pos_enc none 
+python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 3 -pos_enc none 
+python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 3 -pos_enc none 
+python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 3 -pos_enc none 
+python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 3 -pos_enc none 
 
 python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
 python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
@@ -11,18 +11,23 @@ python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 3 -pos_enc 
 python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
 python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
 
-python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 3 -pos_enc none 
-
 python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
 python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
 python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
 python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
 python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
 
+python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
+python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
+python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
+python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
+python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
+
+python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 4 -pos_enc none 
+python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 4 -pos_enc none 
+python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 4 -pos_enc none 
+python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 4 -pos_enc none 
+python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 4 -pos_enc none 
 
 python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 4 -pos_enc delta_days 
 python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 4 -pos_enc delta_days 
@@ -35,12 +40,6 @@ python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 4 -pos_en
 python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
 python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
 python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
-
-python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_a.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_b.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_c.json -exp raw_data -n_clusters 4 -pos_enc none 
 
 python3 main.py -c configs/config_c_f.json -exp raw_data -n_clusters 4 -pos_enc age_pos_enc 
 python3 main.py -c configs/config_c_all.json -exp raw_data -n_clusters 4 -pos_enc age_pos_enc 
