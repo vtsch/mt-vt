58568b75f725d4dcd1f9cc3c617009cd84962987
Vanessa Tschichold <vanessa@tschichold.com>
2022-10-06 00:27:50 +0200

    add script for tstcc supervised

---
+++ run_tstcc_sup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+python3 main.py -c configs/config_c_f.json -exp ts_tcc -n_clusters 2 -pos_enc none  -tstcc_tm supervised
+python3 main.py -c configs/config_c_all.json -exp ts_tcc -n_clusters 2 -pos_enc none -tstcc_tm supervised
+python3 main.py -c configs/config_c_a.json -exp ts_tcc -n_clusters 2 -pos_enc none -tstcc_tm supervised
+python3 main.py -c configs/config_c_b.json -exp ts_tcc -n_clusters 2 -pos_enc none -tstcc_tm supervised
+python3 main.py -c configs/config_c_c.json -exp ts_tcc -n_clusters 2 -pos_enc none -tstcc_tm supervised
+
+python3 main.py -c configs/config_c_f.json -exp ts_tcc -n_clusters 2 -pos_enc absolute_days -tstcc_tm supervised
+python3 main.py -c configs/config_c_all.json -exp ts_tcc -n_clusters 2 -pos_enc absolute_days -tstcc_tm supervised 
+python3 main.py -c configs/config_c_a.json -exp ts_tcc -n_clusters 2 -pos_enc absolute_days -tstcc_tm supervised 
+python3 main.py -c configs/config_c_b.json -exp ts_tcc -n_clusters 2 -pos_enc absolute_days -tstcc_tm supervised 
+python3 main.py -c configs/config_c_c.json -exp ts_tcc -n_clusters 2 -pos_enc absolute_days -tstcc_tm supervised 
+
+python3 main.py -c configs/config_c_f.json -exp ts_tcc -n_clusters 2 -pos_enc delta_days -tstcc_tm supervised
+python3 main.py -c configs/config_c_all.json -exp ts_tcc -n_clusters 2 -pos_enc delta_days -tstcc_tm supervised 
+python3 main.py -c configs/config_c_a.json -exp ts_tcc -n_clusters 2 -pos_enc delta_days -tstcc_tm supervised 
+python3 main.py -c configs/config_c_b.json -exp ts_tcc -n_clusters 2 -pos_enc delta_days -tstcc_tm supervised 
+python3 main.py -c configs/config_c_c.json -exp ts_tcc -n_clusters 2 -pos_enc delta_days -tstcc_tm supervised 
+
+python3 main.py -c configs/config_c_f.json -exp ts_tcc -n_clusters 2 -pos_enc age_pos_enc -tstcc_tm supervised
+python3 main.py -c configs/config_c_all.json -exp ts_tcc -n_clusters 2 -pos_enc age_pos_enc -tstcc_tm supervised 
+python3 main.py -c configs/config_c_a.json -exp ts_tcc -n_clusters 2 -pos_enc age_pos_enc -tstcc_tm supervised
+python3 main.py -c configs/config_c_b.json -exp ts_tcc -n_clusters 2 -pos_enc age_pos_enc -tstcc_tm supervised
+python3 main.py -c configs/config_c_c.json -exp ts_tcc -n_clusters 2 -pos_enc age_pos_enc -tstcc_tm supervised
+
+python3 main.py -c configs/config_c_f.json -exp ts_tcc -n_clusters 2 -pos_enc learnable_pos_enc -tstcc_tm supervised
+python3 main.py -c configs/config_c_all.json -exp ts_tcc -n_clusters 2 -pos_enc learnable_pos_enc -tstcc_tm supervised
+python3 main.py -c configs/config_c_a.json -exp ts_tcc -n_clusters 2 -pos_enc learnable_pos_enc -tstcc_tm supervised
+python3 main.py -c configs/config_c_b.json -exp ts_tcc -n_clusters 2 -pos_enc learnable_pos_enc -tstcc_tm supervised
+python3 main.py -c configs/config_c_c.json -exp ts_tcc -n_clusters 2 -pos_enc learnable_pos_enc -tstcc_tm supervised 
