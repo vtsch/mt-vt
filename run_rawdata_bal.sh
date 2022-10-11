d06edee5fbc21a06c7e7d698209299d651ca7be6
Vanessa Tschichold <vanessa@tschichold.com>
2022-10-11 11:57:12 +0200

    merge scripts for balanced data

--- run_rawdata_bal.sh
+++ run_rawdata_bal.sh
@@ -23,50 +23,32 @@ python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 2 -pos_
 python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 2 -pos_enc age_pos_enc 
 python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 2 -pos_enc age_pos_enc 
 
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 3 -pos_enc none 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 3 -pos_enc none 
+python3 main.py -c configs/config_c_f_bal.json -exp simple_ac -n_clusters 2 -pos_enc none 
+python3 main.py -c configs/config_c_all_bal.json -exp simple_ac -n_clusters 2 -pos_enc none 
+python3 main.py -c configs/config_c_a_bal.json -exp simple_ac -n_clusters 2 -pos_enc none 
+python3 main.py -c configs/config_c_b_bal.json -exp simple_ac -n_clusters 2 -pos_enc none 
+python3 main.py -c configs/config_c_c_bal.json -exp simple_ac -n_clusters 2 -pos_enc none 
 
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 3 -pos_enc absolute_days 
+python3 main.py -c configs/config_c_f_bal.json -exp simple_ac -n_clusters 2 -pos_enc absolute_days 
+python3 main.py -c configs/config_c_all_bal.json -exp simple_ac -n_clusters 2 -pos_enc absolute_days 
+python3 main.py -c configs/config_c_a_bal.json -exp simple_ac -n_clusters 2 -pos_enc absolute_days 
+python3 main.py -c configs/config_c_b_bal.json -exp simple_ac -n_clusters 2 -pos_enc absolute_days 
+python3 main.py -c configs/config_c_c_bal.json -exp simple_ac -n_clusters 2 -pos_enc absolute_days 
 
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 3 -pos_enc delta_days 
+python3 main.py -c configs/config_c_f_bal.json -exp simple_ac -n_clusters 2 -pos_enc age_pos_enc 
+python3 main.py -c configs/config_c_all_bal.json -exp simple_ac -n_clusters 2 -pos_enc age_pos_enc 
+python3 main.py -c configs/config_c_a_bal.json -exp simple_ac -n_clusters 2 -pos_enc age_pos_enc 
+python3 main.py -c configs/config_c_b_bal.json -exp simple_ac -n_clusters 2 -pos_enc age_pos_enc 
+python3 main.py -c configs/config_c_c_bal.json -exp simple_ac -n_clusters 2 -pos_enc age_pos_enc 
 
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 3 -pos_enc age_pos_enc 
+python3 main.py -c configs/config_c_f_bal.json -exp simple_ac -n_clusters 2 -pos_enc delta_days 
+python3 main.py -c configs/config_c_all_bal.json -exp simple_ac -n_clusters 2 -pos_enc delta_days 
+python3 main.py -c configs/config_c_a_bal.json -exp simple_ac -n_clusters 2 -pos_enc delta_days 
+python3 main.py -c configs/config_c_b_bal.json -exp simple_ac -n_clusters 2 -pos_enc delta_days 
+python3 main.py -c configs/config_c_c_bal.json -exp simple_ac -n_clusters 2 -pos_enc delta_days 
 
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 4 -pos_enc none 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 4 -pos_enc none 
-
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 4 -pos_enc absolute_days 
-
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 4 -pos_enc delta_days 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 4 -pos_enc delta_days 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 4 -pos_enc delta_days 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 4 -pos_enc delta_days 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 4 -pos_enc delta_days 
-
-python3 main.py -c configs/config_c_f_bal.json -exp raw_data -n_clusters 4 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_all_bal.json -exp raw_data -n_clusters 4 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_a_bal.json -exp raw_data -n_clusters 4 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_b_bal.json -exp raw_data -n_clusters 4 -pos_enc age_pos_enc 
-python3 main.py -c configs/config_c_c_bal.json -exp raw_data -n_clusters 4 -pos_enc age_pos_enc 
+python3 main.py -c configs/config_c_f_bal.json -exp simple_ac -n_clusters 2 -pos_enc learnable_pos_enc 
+python3 main.py -c configs/config_c_all_bal.json -exp simple_ac -n_clusters 2 -pos_enc learnable_pos_enc 
+python3 main.py -c configs/config_c_a_bal.json -exp simple_ac -n_clusters 2 -pos_enc learnable_pos_enc 
+python3 main.py -c configs/config_c_b_bal.json -exp simple_ac -n_clusters 2 -pos_enc learnable_pos_enc 
+python3 main.py -c configs/config_c_c_bal.json -exp simple_ac -n_clusters 2 -pos_enc learnable_pos_enc 
